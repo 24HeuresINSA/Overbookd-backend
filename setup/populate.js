113b085cbe1e267535fbcde96dac0b77b6420015
Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>
2020-12-21 16:18:33 +0100

    (refactor): 🔨 fixed populate script and added async await syntax for cleaner and more readable code
    Signed-off-by: Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>

--- setup/populate.js
+++ setup/populate.js
@@ -9,36 +9,33 @@ const sequelize = require("../sequelize")
 const models = require("../models/import")(sequelize, Sequelize);
 sequelize
     .sync({force: false})
-    .then(() => {
+    .then(async () => {
         console.log("\nDatabase synced");
 
-        Promise.all([
+        await Promise.all([
             models.Event.bulkCreate(require("./database/event.json")),
             models.Location.bulkCreate(require("./database/location.json")),
             models.Specialty.bulkCreate(require("./database/specialty.json")),
             models.Equipment_Type.bulkCreate(require("./database/equipment_type.json")),
             models.Shift_Category.bulkCreate(require("./database/shift_category.json"))
-        ]).then(() => {
-            Promise.all([
-                models.User.bulkCreate(require("./database/user.json")),
-                models.Equipment.bulkCreate(require("./database/equipment.json")),
-                models.Shift_Category.bulkCreate(require("./database/shift_category.json")),
-                models.Activity.bulkCreate(require("./database/activity.json"))
-            ]).then(() => {
-                Promise.all([
-                    models.Team.bulkCreate(require("./database/team.json")),
-                    models.Task.bulkCreate(require("./database/task.json"))
-                ]).then(() => {
-                    Promise.all([
-                        models.CommentObject.bulkCreate(require("./database/comments.json")),
-                        models.Notification.bulkCreate(require("./database/notification.json"))
-                    ]).then(() => {
-                        console.log("Database populated successfully 🥳");
-                        process.exit(0);
-                    })
-                })
-            })
-        })
+        ])
+        await Promise.all([
+            models.User.bulkCreate(require("./database/user.json")),
+            models.Equipment.bulkCreate(require("./database/equipment.json")),
+            models.Shift_Category.bulkCreate(require("./database/shift_category.json")),
+        ])
+        await Promise.all([
+            models.Activity.bulkCreate(require("./database/activity.json")),
+            models.Team.bulkCreate(require("./database/team.json")),
+        ])
+        await models.Task.bulkCreate(require("./database/task.json"))
+        await Promise.all([
+            models.CommentObject.bulkCreate(require("./database/comments.json")),
+            models.Notification.bulkCreate(require("./database/notification.json"))
+        ])
+
+        console.log("Database populated successfully 🥳");
+        process.exit(0);
     }).catch(err => {
         console.log("Error during syncing :\n" + err);
         process.exit(1);
