30ff603eda4bb5d2bf02159ec5f3acb872939eeb
Hamza <56384031+Azmah-Bad@users.noreply.github.com>
2021-10-04 19:14:27 +0200

    fix(CP): 🔨 fix build issue

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,16 +1,13 @@
 import {IConfig} from "@entities/Config";
-import {Grant} from "keycloak-connect";
 
 declare module 'express' {
     export interface Request  {
-        kauth: {
-            grant: Grant,
-        };
+        kauth?: any;
         body: {
             _id?: string;
             name?: RegExp | string | QuerySelector<RegExp | string> | any;
             keycloakID?: string;
 
-        } | friendRequest | IConfig;
+        } | friendRequest | Config;
     }
 }
