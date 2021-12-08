65995cc9513fcf115855ad81f8d252cdd5c42f6a
Lucas <lucas.zimmermann@protonmail.com>
2021-12-08 19:25:06 +0100

    add byUser property to equipmentProposal

--- src/entities/EquipmentProposal.ts
+++ src/entities/EquipmentProposal.ts
@@ -4,11 +4,13 @@ import { IEquipment } from './Equipment';
 export interface IEquipmentProposal extends IEquipment {
     isNewEquipment: boolean;
     oldEquipment?: Types.ObjectId;
+    byUser: Types.ObjectId;
 }
 
 const EquipmentProposalSchema = new Schema({
     isNewEquipment: { type: Boolean, required: true },
     oldEquipment: { type: Schema.Types.ObjectId, ref: 'Equipment' },   
+    byUser: { type: Schema.Types.ObjectId, ref: 'User' },
 }, { strict: false });
 
 const EquipmentProposalModel = model<IEquipmentProposal>('EquipmentProposal', EquipmentProposalSchema);
